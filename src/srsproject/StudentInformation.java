5b75eab94b4fdf365a5a2fcd3a7a16ae9f08879d
vanessaramos0620 <ramos.vanessa0620@gmail.com>
2024-06-29 11:01:20 +0800

    StudentInfo

--- src/srsproject/StudentInformation.java
+++ src/srsproject/StudentInformation.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package srsproject;
 
 import java.awt.BorderLayout;
@@ -184,7 +181,6 @@ public class StudentInformation extends JFrame implements ActionListener {
             }
         }
 }
-
 
 
     
